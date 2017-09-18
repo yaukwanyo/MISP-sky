77db213ac9d65073af291a43fbf9670ec4128244
yaukwanyo <y16593@yahoo.com.hk>
2017-09-18 09:29:09 +0200

    Changes to be committed: 	new file:   DomainIPs_CSV_export.py 	new file:   File_CSV_export.py

---
+++ misp_modules/modules/export_mod/DomainIPs_CSV_export.py
@@ -0,0 +1,121 @@
+import json
+import base64
+import datetime
+import csv
+import io
+
+misperrors = {'error': 'Error'}
+
+# possible module-types: 'expansion', 'hover' or both
+moduleinfo = {'version': '1', 'author': 'Hannah Ward',
+              'description': 'Export domain/ip scan results in csv format',
+              'module-type': ['export']}
+
+# config fields that your code expects from the site admin
+
+
+fieldmap = {
+    "domain": "Domains/IPs",
+    "hostname": "Domain/IPs",
+    "ip-src": "Domain/IPs",
+    "ip-dst": "Domain/IPs",
+    "url": "Domain/IPs"
+}
+
+mispattributes = {'input':list(fieldmap.keys())}
+outputFileExtension = "csv"
+responseType = "application/txt"
+
+def handler(q=False):
+    if q is False:
+        return False
+    request = json.loads(q)
+
+    print(request)
+    response = io.StringIO()
+    writer = csv.DictWriter(response, fieldnames=["Type", "Value", "Virustotal Detection Ratio", "Quttera.com", "Sucuri",  "Port status"])
+
+    writer.writeheader()
+   # csv.QUOTE_ALL
+    for event in request["data"]:
+        for attribute in event["Attribute"]:
+            if attribute["type"] in mispattributes["input"]:
+                writer.writerow({
+                    "Type": fieldmap[attribute["type"]],
+                    "Value": attribute["value"],
+                    "Virustotal Detection Ratio": getvtResult(attribute["comment"]),
+                    "Quttera.com": getQutteraResult(attribute["comment"]),
+                    "Sucuri": getSucuriResult(attribute["comment"]),
+                    "Port status": getsignal(attribute["comment"])
+                })
+    #csv.QUOTE_ALL                   
+    r = {"response":[], "data":str(base64.b64encode(bytes(response.getvalue(), 'utf-8')), 'utf-8')}
+    return r
+
+def st(comment, keyword):
+    diff = len(keyword)
+    stPos = comment.find(keyword) + diff
+    return stPos
+
+def getsignal(comment):
+    stPos = st(comment, "Port 80: ")
+    endPos = comment.find(" Port 443")
+    p80 = comment[stPos:endPos]
+    stPos = st(comment, "Port 443: ")
+    p443 = comment[stPos:]
+    result = "Port 80: \r\n" + p80 + "\r\n\r\nPort 443: " + p443
+    return result
+
+def getSucuriResult(comment):
+    stPos = st(comment,"Status: ")
+    endPos = comment.find(" Web Trust")
+    status = comment[stPos:endPos]
+    stPos = st(comment, "Web Trust: ")
+    endPos = comment.find("Port Status")
+    webTrust = comment[stPos:endPos]
+    sucuri = "Status: \r\n" + status + "\r\n\r\nWeb Trust: \r\n" + webTrust
+    return sucuri
+
+def getQutteraResult(comment):
+    stPos = st(comment, "Quttera Result: ")
+    endPos = comment.find(" Sucuri") 
+    quttera = comment[stPos:endPos]
+    return quttera
+
+def getvtResult(comment):
+    stPos = st(comment, "tio: ")
+    endPos = comment.find(" Quttera")
+    vt = "'" + comment[stPos:endPos]
+    return vt
+
+#def getSucuriResult(comment):
+    #stPos = comment.find(
+
+def introspection():
+  modulesetup = {}
+  try:
+        responseType
+        modulesetup['responseType'] = responseType
+  except NameError:
+      pass
+  try:
+      userConfig
+      modulesetup['userConfig'] = userConfig
+  except NameError:
+      pass
+  try:
+      outputFileExtension
+      modulesetup['outputFileExtension'] = outputFileExtension
+  except NameError:
+      pass
+  try:
+      inputSource
+      modulesetup['inputSource'] = inputSource
+  except NameError:
+      pass
+  return modulesetup
+
+def version():
+    #moduleinfo['config'] = moduleconfig
+    return moduleinfo
+
